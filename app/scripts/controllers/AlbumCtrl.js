08ade978fd2ac783838105dff98ee1f2f1168ef5
Shawn Thottukandathil <shawn.thott@gmail.com>
2017-05-17 14:12:00 -0500

    assignment 5 complete 2

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,12 @@
+(function() {
+     function AlbumCtrl() {
+        this.albumData = angular.copy(albumPicasso)
+        }
+
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', AlbumCtrl);
+    
+ })();
